5dbc26816ce0b26d6e101b07023ece7db933508f
Leena-Yemul <leena.yemul@gmail.com>
2025-02-22 13:26:37 +0530

    new version

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import { Container, Navbar, Nav } from 'react-bootstrap';
 import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
-import Aboutus from './pages/Aboutus';
-import Qualification from './pages/Qualification';
-import Skills from './pages/Skills';
-import Projects from './pages/Projects';
-import Contactus from './pages/Contactus';
-// import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
-import styled from "styled-components";
-
+import View_qualification from './Views/View_qualification';
+import View_skills from './Views/View_skills';
+import View_contact from './Views/View_contact';
+import View_about from './Views/View_about';
+import View_projects from './Views/View_projects';
 function App() {
   return (
     <>
     <BrowserRouter>
      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/aboutus" element={<Aboutus />} />
-        <Route path="/qualification" element={<Qualification />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contactus" element={<Contactus />} />
+        <Route path="/aboutus" element={<View_about/>} />
+        <Route path="/qualification" element={<View_qualification />} />
+        <Route path="/skills" element={<View_skills />} />
+        <Route path="/projects" element={<View_projects/>} />
+        <Route path="/contactus" element={<View_contact/>} />
       </Routes>
     </BrowserRouter>
     </>
